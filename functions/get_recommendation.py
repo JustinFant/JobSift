45c6ccb34d6c43868ea6e8384369fff6486deba4
JustinFant <justin.fant@bepcinc.com>
2023-12-18 11:36:36 -0600

    .

--- functions/get_recommendation.py
+++ functions/get_recommendation.py
@@ -1,7 +1,7 @@
 def get_recommendation(overall_score) -> str:
-    if overall_score >= 8:
+    if overall_score > 7:
         return 'Candidate is a great fit.'
-    elif overall_score >= 6:
+    elif overall_score >= 5:
         return 'Candidate is a good fit.'
     else:
         return 'Candidate is not a good fit.'
