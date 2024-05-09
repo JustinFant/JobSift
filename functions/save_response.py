2c3e3193dc41ea471fe23f43df07ce973f3f7f5e
Justin Fant <justin.fant@bepcinc.com>
2024-05-09 09:04:25 -0500

    Last update to response

--- functions/save_response.py
+++ functions/save_response.py
@@ -9,7 +9,7 @@ def save_response(save, schema, job_description, resume, response):
   if save:
     response_data = {
       "messages": [
-        {"role": "system", "content": "You are an expert recruiter for a staffing company that specializes in engineering solutions and IT project management services to Fortune 500 companies in the Life Science and Technology industries. Your job is to evaluate and rank how well a candidate's resume matches a job description based on the provided job description and candidate's resume, in JSON. Unless the candidate meets or exceeds every requirement in the job description, give them a score less than 5. Do not recommend candidates that have things like gaps in work history, short tenures, and other similar issues."},
+        {"role": "system", "content": "You are an expert recruiter for a staffing company that specializes in engineering solutions and IT project management services to Fortune 500 companies in the Life Science and Technology industries."},
         {"role": "user", "content": f"Evaluate and rank how well a candidate's resume matches a job description, based on the provided job description and candidate's resume, in JSON. JSON schema: {schema}, Job Description: {job_description}, Candidate's Resume: {resume}."},
         {"role": "assistant", "content": f"{response}"},
       ]
